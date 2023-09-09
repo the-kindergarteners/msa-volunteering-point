2145c2ab1960fd64240d5a1a71205c9fc57e7132
Mumtaz Aziz <mumtazaziz020305@gmail.com>
2023-09-10 03:30:56 +1000

    Fix linting

--- app/api/events/route.ts
+++
@@ -1,5 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function GET (request: Request): Promise<Response> {
-  return NextResponse.json([{ name: 'O-Week', date: new Date(), jobsLeft: 0 }])
-}
