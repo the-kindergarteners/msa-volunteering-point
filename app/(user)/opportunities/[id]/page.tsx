ab9e9cf6c3f71f211dfcc50767563bd19f6fc905
Mumtaz Aziz <mumtazaziz020305@gmail.com>
2023-09-10 04:18:25 +1000

    Fix generateStaticParams()

--- app/(user)/opportunities/[id]/page.tsx
+++ app/(user)/opportunities/[id]/page.tsx
@@ -2,11 +2,11 @@ import { Container } from '@/utils/bootstrap'
 import getEvents from '@/utils/get-events'
 import getEvent from '@/utils/get-event'
 
-export async function generateStaticParams() {
+export async function generateStaticParams (): Promise<{ id: string }[]> {
   const events = await getEvents()
- 
+
   return events.map(event => ({
-    id: event.id,
+    id: event.id
   }))
 }
 
